7ab93651a79d7aaaa928f408d0b793161c8d45a3
CallumMcLoughlin <59436970+CallumMcLoughlin@users.noreply.github.com>
2022-08-08 12:53:25 +1200

    Remove redundant includes

--- DTLS/include/common/info.h
+++ DTLS/include/common/info.h
@@ -1,13 +1,6 @@
 #ifndef PQDTLS_INFO_H
 #define PQDTLS_INFO_H
 
-#if WIN32
-    #define WOLFSSL_USER_SETTINGS
-#endif
-
-#include <wolfssl/wolfcrypt/settings.h>
-#include <wolfssl/ssl.h>
-
 #include "client/client.h"
 
 void info_print_ssl_summary(WOLFSSL* con);
