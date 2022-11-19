8e859739c82a28c56901c3adf395403bbce60431
CallumMcLoughlin <59436970+CallumMcLoughlin@users.noreply.github.com>
2022-11-19 18:12:09 +1300

    Update usage monitoring script to be based of UNIX time

--- WinTools/DTLS/usage.sh
+++ WinTools/DTLS/usage.sh
@@ -4,5 +4,14 @@ top -b | head -n7
 
 while true
 do
-    top -b -n 1 -d 0.05 -p $(pidof dtls_server) 2>/dev/null | tail -1
+    if pidof dtls_server; then
+        break
+    fi
+    sleep 0.1
+done
+
+while true
+do
+    { date "+%s.%N"; top -b -d 0.05 -n 1 -p $(pidof dtls_server) | tail -n1; } | tr "\n" " " | sed '$s/ $/\n/'
+    sleep 0.05
 done
